3cbb6bc64d6616416f4744aedb4eb6f17e024a68
Schylle <schyllepalmero@gmail.com>
2026-06-19 11:17:30 +0800

    Update pubspec.yaml and generated plugin files

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,17 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
-<<<<<<< HEAD
-=======
-#include <firebase_core/firebase_core_plugin_c_api.h>
->>>>>>> c28eecc (Initial commit)
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
-<<<<<<< HEAD
-=======
-  FirebaseCorePluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
->>>>>>> c28eecc (Initial commit)
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,10 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
-<<<<<<< HEAD
-=======
-  firebase_core
->>>>>>> c28eecc (Initial commit)
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
